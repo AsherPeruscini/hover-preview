a5866a32309af5619a104c9ca4eb9b745df22c97
Asher Peruscini <asherperuscini@gmail.com>
2016-05-13 15:47:47 -0700

    added ported files + atom package generator contents

---
+++ spec/hover-preview-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import HoverPreviewView from '../lib/hover-preview-view';
+
+describe('HoverPreviewView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
